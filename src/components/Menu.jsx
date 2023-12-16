36ae809f476ac54a7d73364e29ba78cc7d471ad2
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-12-17 00:29:15 +0545

    Layout Menu component

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,9 +1,25 @@
+import "../styles/Menu.css";
+
+function StartButton() {
+  return <></>;
+}
+
+function GameDescription() {
+  return <></>;
+}
+
+function Settings() {
+  return <div className="settings"></div>;
+}
+
 export default function Menu() {
   return (
     <>
       <div className="menu">
-        <h1>TITLE</h1>
-        <h2>Play</h2>
+        <h1 className="game-title">Meeemorrieeze</h1>
+        <Settings></Settings>
+        <StartButton></StartButton>
+        <GameDescription></GameDescription>
       </div>
     </>
   );
