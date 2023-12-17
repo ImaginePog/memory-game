6f0132ebd92f40b4736afa26eafb1b57dc0c945f
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-12-17 22:27:48 +0545

    Add utility function to randomly shuffle an array

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -3,3 +3,7 @@ export function getRandomInt(min, max) {
   max = Math.floor(max);
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
+
+export function shuffleArray(array) {
+  return array.sort(() => Math.random() - 0.5);
+}
