a5f186a5ea876d64f23dc22dc64ab1795b7ed643
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-12-17 16:45:43 +0545

    Create data.js for storing predefined data (currently temp data)

---
+++ src/utils/data.js
@@ -0,0 +1,40 @@
+export const categories = [
+  {
+    name: "Pokemon",
+    imgSrc: "...",
+    description: "Random pokemons from all generations",
+  },
+  {
+    name: "Rick and Morty",
+    imgSrc: "...",
+    description: "Random rick and morty characters",
+  },
+  {
+    name: "Dogs",
+    imgSrc: "...",
+    description: "Random dog images of different breeds",
+  },
+];
+
+export const difficultySettings = [
+  {
+    difficulty: "Goldfish",
+    cards: 5,
+    description: "Haha goldfish memory only 5 pairs of cards",
+  },
+  {
+    difficulty: "Elephant",
+    cards: 10,
+    description: "Commendable memory, 10 pairs of cards",
+  },
+  {
+    difficulty: "Dolphin",
+    cards: 20,
+    description: "Awesome memory, 20 pairs of cards",
+  },
+  {
+    difficulty: "Savant",
+    cards: 50,
+    description: "A Savant :o, 50 pairs of cards from all different contents.",
+  },
+];
