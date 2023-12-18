570feca02154f4f78885deffd3aef56c523321de
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-12-18 17:22:47 +0545

    Add time values for difficulties

--- src/utils/data.js
+++ src/utils/data.js
@@ -8,7 +8,7 @@ export const categories = {
     description: "Random rick and morty characters",
   },
   Dogs: {
-    imgSrc: "/public/images/pokemon-card.png",
+    imgSrc: "/public/images/dogs-card.jpg",
     description: "Random dog images of different breeds",
   },
 };
@@ -16,19 +16,25 @@ export const categories = {
 export const difficulties = {
   Goldfish: {
     cards: 5,
-    description: "Haha goldfish memory only 5 pairs of cards",
+    description: "Haha goldfish memory only 5 pairs of cards in 3 minutes",
+    time: 180, //In seconds
   },
   Elephant: {
     cards: 10,
-    description: "Commendable memory, 10 pairs of cards",
+    description:
+      "Commendable memory, 10 pairs of cards in 1 and a half minutes",
+    time: 90, //In seconds
   },
   Dolphin: {
     cards: 20,
-    description: "Awesome memory, 20 pairs of cards",
+    description: "Awesome memory, 20 pairs of cards in 2 minutes",
+    time: 120, //In seconds
   },
   Savant: {
     cards: 50,
-    description: "A Savant :o, 50 pairs of cards from all different contents.",
+    description:
+      "A Savant :o, 50 pairs of cards from all different contents in 1 minute",
+    time: 60,
   },
 };
 
