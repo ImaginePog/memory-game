c2a9eee0bda5e4f0d5a965c142f760a39e1f005e
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-12-18 18:17:28 +0545

    Extract fetch calls to a different file for organizing

--- src/components/LoadingScreen.jsx
+++ src/components/LoadingScreen.jsx
@@ -1,117 +1,15 @@
+// React imports
 import { useEffect } from "react";
 
+// Other imports
 import { difficulties } from "../utils/data";
-import { getRandomInt, shuffleArray } from "../utils/utils";
+import { getRandomInt } from "../utils/utils";
 
-async function getPokemonById(id) {
-  const pokemonResponse = await fetch(
-    `https://pokeapi.co/api/v2/pokemon/${id}/`
-  );
-  const pokemonJSON = await pokemonResponse.json();
-  const { name, sprites } = { ...pokemonJSON };
-
-  return {
-    name,
-    imageSrc: sprites.other["official-artwork"]["front_default"],
-  };
-}
-
-async function getPokemonList(nPokemon) {
-  const pokemonCountRes = await fetch(
-    "https://pokeapi.co/api/v2/pokemon-species/?limit=0"
-  );
-  const pokemonCountJSON = await pokemonCountRes.json();
-
-  const usedIds = [];
-  const totalPokemonNumber = pokemonCountJSON.count;
-
-  const pokemonList = [];
-
-  for (let i = 0; i < nPokemon; ++i) {
-    let randomPokemonId = getRandomInt(1, totalPokemonNumber);
-    while (usedIds.includes(randomPokemonId)) {
-      randomPokemonId = getRandomInt(1, totalPokemonNumber);
-    }
-    usedIds.push(randomPokemonId);
-    const pokemon = await getPokemonById(randomPokemonId);
-    pokemonList.push(pokemon);
-  }
-
-  return pokemonList;
-}
-
-async function getRickMortyCharacters(page = 1) {
-  const charactersRes = await fetch(
-    `https://rickandmortyapi.com/api/character/?status=alive&page=${page}`
-  );
-  const charactersJSON = await charactersRes.json();
-  const results = charactersJSON.results;
-
-  // Filter names with 3+ words or 15+ letters
-  const filtered = results.filter(
-    (result) => result.name.split(" ").length < 3 && result.name.length < 15
-  );
-
-  return filtered;
-}
-
-async function getRickMortyList(nCharacters) {
-  const pageRes = await fetch(
-    "https://rickandmortyapi.com/api/character/?status=alive"
-  );
-  const pageJSON = await pageRes.json();
-  const totalPages = pageJSON.info.pages;
-
-  const usedPages = [];
-
-  let randomPage = getRandomInt(1, totalPages);
-  let rawResults = await getRickMortyCharacters(randomPage);
-  usedPages.push(randomPage);
-
-  while (rawResults.length < nCharacters) {
-    // If current results are less than required characters
-    while (usedPages.includes(randomPage)) {
-      randomPage = getRandomInt(1, totalPages);
-    }
-    usedPages.push(randomPage);
-    const newResults = await getRickMortyCharacters(randomPage);
-    rawResults.push(...newResults);
-  }
-
-  rawResults = shuffleArray(rawResults);
-  rawResults.length = nCharacters;
-
-  let characterList = [];
-  rawResults.forEach((result) => {
-    characterList.push({ name: result.name, imageSrc: result.image });
-  });
-
-  return characterList;
-}
-
-async function getDogsList(nDogs) {
-  const dogsRes = await fetch(
-    `https://dog.ceo/api/breeds/image/random/${nDogs}`
-  );
-  const dogsJSON = await dogsRes.json();
-
-  const results = dogsJSON.message.map((result) => {
-    // Find the breed name in the image link
-    // Split the names with '-' (Api uses '-' between words for directory)
-    // Change each word to uppercase
-    // Join back the string for the dog's breed
-    let startPos = result.indexOf("breeds/") + "breeds/".length;
-    const name = result
-      .substring(startPos, result.indexOf("/", startPos))
-      .split("-")
-      .map((word) => word.charAt(0).toUpperCase() + word.substring(1))
-      .join(" ");
-
-    return { name, imageSrc: result };
-  });
-
-  return results;
-}
+import {
+  getPokemonList,
+  getRickMortyList,
+  getDogsList,
+} from "../utils/fetchers";
 
 function divideCards(nCards, categories) {
   const nCategories = categories.length;
