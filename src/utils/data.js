5f616a2079d660d7ac902615b130cd2038542ee0
imagine <91915381+ImaginePog@users.noreply.github.com>
2023-12-20 00:01:34 +0545

    Add dimension of cards to difficulties

--- src/utils/data.js
+++ src/utils/data.js
@@ -18,23 +18,31 @@ export const difficulties = {
     cards: 5,
     description: "Haha goldfish memory only 5 pairs of cards in 3 minutes",
     time: 180, //In seconds
+    rows: 2,
+    cols: 5,
   },
   Elephant: {
     cards: 10,
     description:
       "Commendable memory, 10 pairs of cards in 1 and a half minutes",
     time: 90, //In seconds
+    rows: 4,
+    cols: 5,
   },
   Dolphin: {
     cards: 20,
     description: "Awesome memory, 20 pairs of cards in 2 minutes",
     time: 120, //In seconds
+    rows: 8,
+    cols: 5,
   },
   Savant: {
     cards: 50,
     description:
       "A Savant :o, 50 pairs of cards from all different contents in 1 minute",
     time: 60,
+    rows: 10,
+    cols: 10,
   },
 };
 
